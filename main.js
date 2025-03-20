cda6f91ea63ee210a0182dcd7d668fa1fd94070c
KartoffelMad058 <142486593+KartoffelMad058@users.noreply.github.com>
2025-03-20 12:30:08 +0100

    bordene bliver vist
    De bliver vist når man væglger hvor mange personer man er, og hvilken tid man kan komme

--- main.js
+++ main.js
@@ -12,9 +12,53 @@ function tjektid(tid) {
 }
 
 function valg() {
+    
     if (number != 0 && time != "00:00") { 
-        window.location.href="/bookbord/vælgbord.html";
+        localStorage.setItem("number", number);
+        /*window.location.href="/bookbord/vælgbord.html";*/
+        visknap(number);
+
     } else {
         alert("Vælg venligst antal personer og tidspunkt");
     }
 } 
+
+
+
+function visknap(number){
+    skjulknap();
+    /*alert("virker stadig");*/
+
+    if((number == 1) || (number ==2)
+    ){
+        document.getElementById("k1").classList.remove("hidden");
+        document.getElementById("k2").classList.remove("hidden");
+        document.getElementById("k3").classList.remove("hidden");
+        document.getElementById("k4").classList.remove("hidden");
+    
+    } else if(number == 3 || number == 4){
+        document.getElementById("k5").classList.remove("hidden");
+        document.getElementById("k6").classList.remove("hidden");
+        document.getElementById("k7").classList.remove("hidden");
+        document.getElementById("k8").classList.remove("hidden");
+
+    } else if(number == 5 || number ==6){
+        document.getElementById("k9").classList.remove("hidden");
+        document.getElementById("k10").classList.remove("hidden");
+    }
+
+}
+
+function skjulknap(){
+    document.getElementById("k1").classList.add("hidden");
+    document.getElementById("k2").classList.add("hidden");
+    document.getElementById("k3").classList.add("hidden");
+    document.getElementById("k4").classList.add("hidden");
+    document.getElementById("k5").classList.add("hidden");
+    document.getElementById("k6").classList.add("hidden");
+    document.getElementById("k7").classList.add("hidden");
+    document.getElementById("k8").classList.add("hidden");
+    document.getElementById("k9").classList.add("hidden");
+    document.getElementById("k10").classList.add("hidden");
+}
+
