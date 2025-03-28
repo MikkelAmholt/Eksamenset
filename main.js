9c422242327d07dc92eb5f5677516aa53b76b6d2
KartoffelMad058 <142486593+KartoffelMad058@users.noreply.github.com>
2025-03-28 11:16:37 +0100

    Bedre fix
    rg wo

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 import { serveDir } from 'jsr:@std/http/file-server';
-import { Database } from 'jsr:@db/sqlite';
-
-const db = new Database(':memory:');
-
-db.exec(
-  'CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT not null, sirName TEXT not null,age INTEGER not null)'
-);
 
 Deno.serve(async (req) => {
   const pathname = new URL(req.url).pathname;
 
-  if (pathname == '/api/people') {
-    return Response.json(db.prepare('select * from people').all());
+  if (pathname == '/api/bookings') {
+    const file = Deno.readTextFileSync("db.json")
+    return new Response(file);
   }
 
   if (pathname == '/api/booking') {
-    const formData = await req.formData();'
+    const formData = await req.formData();
     
     const navn= formData.get("navn");
     const telefon = formData.get("telefon");
