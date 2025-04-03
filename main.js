7666c64576b886eee0a51320f039e6a16b618666
KartoffelMad058 <142486593+KartoffelMad058@users.noreply.github.com>
2025-04-03 12:32:57 +0200

    nu kan man ses hvilke borde, som er reseveret
    Det er ikke så flot, men man kan se det

--- main.js
+++ main.js
@@ -4,45 +4,52 @@ Deno.serve(async (req) => {
   const pathname = new URL(req.url).pathname;
 
   if (pathname == '/api/bookings') {
-    const file = Deno.readTextFileSync("db.json")
-    return new Response(file);
+    const file = Deno.readTextFileSync("db.json");
+    const data = JSON.parse(file);
+
+    return new Response(JSON.stringify(data, null, 4), {  // Formatteret JSON
+      headers: { "Content-Type": "application/json" }
+    });
   }
 
-
-  if(pathname == "/api/delete_booking") {
+  if (pathname == "/api/delete_booking") {
     const formData = await req.formData();
 
     const bordId = formData.get("bordId");
     const tid = formData.get("tid");
-    console.log("delte>",bordId,tid);
+    console.log("delte>", bordId, tid);
 
-    const file = Deno.readTextFileSync("db.json")
-    const data = JSON.parse(file)
+    const file = Deno.readTextFileSync("db.json");
+    const data = JSON.parse(file);
 
     data.bord[bordId].reservationer = data.bord[bordId].reservationer.filter(reservation => reservation.tid !== tid);
 
-    Deno.writeTextFileSync("db.json", JSON.stringify(data))
+    Deno.writeTextFileSync("db.json", JSON.stringify(data, null, 4));
 
-    return new Response(null)
+    return new Response(JSON.stringify(data, null, 4), { // Returnerer opdateret JSON
+      headers: { "Content-Type": "application/json" }
+    });
   }
 
   if (pathname == '/api/booking') {
     const formData = await req.formData();
     
-    const navn= formData.get("navn");
+    const navn = formData.get("navn");
     const telefon = formData.get("telefon");
     const tid = formData.get("tid");
     const antal = formData.get("antal");
     const bordId = formData.get("bordId");
-     
 
-    const file = Deno.readTextFileSync("db.json")
-    const data = JSON.parse(file)
+    const file = Deno.readTextFileSync("db.json");
+    const data = JSON.parse(file);
 
-    data.bord[bordId].reservationer.push({"navn": navn, "telefon": telefon, "tid": tid, "antal": antal})
+    data.bord[bordId].reservationer.push({ "navn": navn, "telefon": telefon, "tid": tid, "antal": antal });
 
-    Deno.writeTextFileSync("db.json", JSON.stringify(data))
-    return new Response(null)
+    Deno.writeTextFileSync("db.json", JSON.stringify(data, null, 4));
+
+    return new Response(JSON.stringify(data, null, 4), { // Returnerer opdateret JSON
+      headers: { "Content-Type": "application/json" }
+    });
   }
 
   return serveDir(req, {
@@ -50,5 +57,3 @@ Deno.serve(async (req) => {
     showIndex: true,
   });
 });
-
-
